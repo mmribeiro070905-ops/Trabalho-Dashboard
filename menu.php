52c252cfc0fb52a87f6d57b212740807339f7ae4
Murilo Ribeiro <mmribeiro070905@gmail.com>
2026-03-23 22:51:52 -0300

    new

---
+++ menu.php
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li><a href="index.php"><i class="fa-solid fa-house"></i> Início</a></li>
+        <li><a href="noticias.php"><i class="fa-solid fa-newspaper"></i> Notícias</a></li>
+        <li><a href="dicas.php"><i class="fa-solid fa-gamepad"></i> Dicas</a></li>
+        <li><a href="usuarios.php"><i class="fa-solid fa-users"></i> Usuários</a></li>
+        <li><a href="cad-usuarios.php"><i class="fa-solid fa-user-plus"></i> Cadastrar</a></li>
+        <li><a href="postagens.php"><i class="fa-regular fa-pen-to-square"></i> Postagens</a></li>
+        <li><a href="categorias.php"><i class="fa-solid fa-list"></i> Categorias</a></li>
+        
+    </ul>
+
+    <div class="perfil-usuario">
+        <img src="img/js.webp" alt="Avatar">
+        <span>Murilo Ribeiro</span>
+    </div>
+</nav>
