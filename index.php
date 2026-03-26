5cd558136c9c67eb0c79f636b1deae005adbda7d
Murilo Ribeiro <mmribeiro070905@gmail.com>
2026-03-26 16:19:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="login-container">
+        <h1>Login</h1>
+        <form>
+            <input type="email" placeholder="Digite seu e-mail" required>
+            <input type="password" placeholder="Digite sua senha" required>
+
+            <td colspan="6" align="right"><a href="dashboard.php" class="btn-add"><i class="fa-solid fa-plus"></i> Entrar</a>
+        </form>
+    </div>
+</body>
+</html>
