55f518869c0e2a67445e264e011c1c3e805db78c
Murilo Ribeiro <mmribeiro070905@gmail.com>
2026-03-27 17:33:33 -0300

    Update postagens.php

--- postagens.php
+++ postagens.php
@@ -11,9 +11,9 @@
     <div class="dashboard-container">
         <nav>
                 <ul>
-                    <li><a href="dashboard.php">Início</a></li>
-                    <li><a href="usuarios.php">Usuários</a></li>
-                    <li><a href="https://arsenaldirect.arsenal.com/Football-Shirts-and-Kit/c/kit">Produtos</a></li>
+                    <li><a href="#"><i class="fas fa-home"></i> Início</a></li>
+                    <li><a href="#"><i class="fas fa-user"></i> Usuários</a></li>
+                    <li><a href="https://arsenaldirect.arsenal.com/Football-Shirts-and-Kit/c/kit"><i class="fas fa-box"></i> Produtos</a></li>
                 </ul> 
 
                 <div class="perfil-usuario">
