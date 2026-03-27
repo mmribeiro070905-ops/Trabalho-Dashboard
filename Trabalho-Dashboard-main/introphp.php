4598e0525cd99cddf24d4418852e465838bb3402
Murilo Ribeiro <mmribeiro070905@gmail.com>
2026-03-27 20:25:49 -0300

    Delete Trabalho-Dashboard-main directory

--- Trabalho-Dashboard-main/introphp.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $nome = "Murilo";
-    $idade = "20";
-    echo "Olá, ".$nome. "voce tem ".$idade." anos";
-?>
--- Trabalho-Dashboard-main/rodape.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Murilo Ribeiro - Desenvolvido na aula de Web I</p>
-</footer>
