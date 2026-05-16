0f136aa20ade93e6e254a60220d66b2102a3e6de
Murilo Ribeiro <mmribeiro070905@gmail.com>
2026-05-15 21:30:39 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
   <nav>
             <ul>
-                <li><a href="#">Início</a></li>
+                <li><a href="dashboard.php">Início</a></li>
                 <li><a href="https://www.arsenal.com/news">Notícias</a></li>
                 <li><a href="https://www.arsenal.com/fixtures">Jogos</a></li>
                 <li><a href="https://www.arsenal.com/men/players">Elenco</a></li>
